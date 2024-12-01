f613b3d0a856e9eb799ab42560bda603718b0b42
taylorperkins <taylorperkins.dev@gmail.com>
2024-12-01 12:32:46 -0600

    Part 1

--- day_1/part_1.py
+++ day_1/part_1.py
@@ -1,9 +1,29 @@
+import re
+
 from utils import time_it, read_input, input_path
+
+
+pattern = re.compile(r'^(?P<n1>\d+)\s+(?P<n2>\d+)?$')
 
 
 @time_it
 def main(data: str) -> str:
-    return data
+
+    v1, v2 = [], []
+    for line in data.splitlines():
+        m = pattern.match(line)
+        if not m:
+            raise Exception("Bad Pattern, ", line)
+
+        n1, n2 = m.group("n1"), m.group("n2")
+        v1.append(int(n1)), v2.append(int(n2))
+
+    distance = 0
+    for l, r in zip(sorted(v1), sorted(v2)):
+        d = abs(l - r)
+        distance += d
+
+    return distance
 
 
 if __name__ == "__main__":
