2c79d1fa0b76062c3b77ad779d3c91ab56e792bf
taylorperkins <taylorperkins.dev@gmail.com>
2024-12-02 07:21:44 -0600

    part 1

--- day_2/part_1.py
+++ day_2/part_1.py
@@ -1,9 +1,32 @@
+from typing import Callable, List
+
 from utils import time_it, read_input, input_path
 
 
+def ascending(a: int, b: int) -> bool: return b > a
+def descending(a: int, b: int) -> bool: return a > b
+def differ(l: int, r: int) -> bool: return 1 <= abs(l - r) <= 3
+
+
+def check(level: List[int]) -> bool:
+    """Checks whether a level is 'safe' (true) or 'unsafe' (false)"""
+    direction = ascending if level[0] < level[1] else descending
+
+    for i in range(len(level) - 1):
+        l, r = level[i], level[i+1]
+        if not (direction(l, r) and differ(l, r)):
+            return False
+    return True
+
+
 @time_it
-def main(data: str) -> str:
-    return data
+def main(data: str) -> int:
+    safe = 0
+    for line in data.splitlines():
+        level = [int(v) for v in line.split(" ")]
+        safe += check(level)
+
+    return safe
 
 
 if __name__ == "__main__":
