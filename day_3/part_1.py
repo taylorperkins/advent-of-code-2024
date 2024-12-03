f2ca5e178f4a92abb7f74e079fed2506e3eb26b6
taylorperkins <taylorperkins.dev@gmail.com>
2024-12-03 07:43:39 -0600

    Part 1

--- day_3/part_1.py
+++ day_3/part_1.py
@@ -1,9 +1,21 @@
+import re
+
 from utils import time_it, read_input, input_path
 
 
+pattern = re.compile(r'mul\((?P<n1>\d+),(?P<n2>\d+)\)')
+
+
 @time_it
-def main(data: str) -> str:
-    return data
+def main(data: str) -> int:
+    total = 0
+    for line in data.splitlines():
+        total += sum(
+            int(m[0]) * int(m[1])
+            for m in pattern.findall(line)
+        )
+
+    return total
 
 
 if __name__ == "__main__":
