f9b50936b85c8bf6c9b1ca58c2243835473659c3
taylorperkins <taylorperkins.dev@gmail.com>
2024-12-11 08:22:27 -0600

    Part 2

--- day_11/part_2.py
+++ day_11/part_2.py
@@ -1,9 +1,37 @@
+from collections import Counter
+from typing import Dict
+
 from utils import time_it, read_input, input_path
 
 
+def blink(stones: Dict[str, int]) -> Dict[str, int]:
+    c = Counter()
+
+    for stone, count in stones.items():
+        if stone == "0":
+            c["1"] += count
+
+        else:
+            size = len(stone)
+            if size % 2 == 0:
+                mid = size // 2
+                left, right = int(stone[:mid]), int(stone[mid:])
+                c[str(left)] += count
+                c[str(right)] += count
+
+            else:
+                c[str(int(stone)*2024)] += count
+
+    return c
+
+
 @time_it
-def main(data: str) -> str:
-    return data
+def main(data: str) -> int:
+    c = Counter(data.split(" "))
+    for _ in range(75):
+        c = blink(c)
+
+    return sum(c.values())
 
 
 if __name__ == "__main__":
