30f76e94ab4c468d6f13dff1e884285e9833ed5b
taylorperkins <taylorperkins.dev@gmail.com>
2024-12-19 21:02:21 -0600

    Day 19 refactor

--- day_19/part_2.py
+++ day_19/part_2.py
@@ -1,12 +1,22 @@
 import re
 from collections import defaultdict
+from functools import reduce
 
-from typing import List, Dict, Set
+from typing import Generator, Tuple
 
 from utils import time_it, read_input, input_path
 
 
-def match(patterns: Set[str], desired_pattern: str) -> int:
+def match(patterns: Generator[Tuple[str, int], None, None], desired_pattern: str) -> int:
+
+    index_starts = defaultdict(lambda : defaultdict(int))
+    for p, size in patterns:
+        for m in re.finditer(r'(?=(' + p + '))', desired_pattern):
+            index_starts[m.start()][size] += 1
+
+    if not index_starts[0]:
+        return 0
+
     # start at the beginning
     idx = 0
 
@@ -16,20 +26,17 @@ def match(patterns: Set[str], desired_pattern: str) -> int:
 
     # indices to check along the way
     stops = {idx}
+    desired_pattern_size = len(desired_pattern)
 
-    while idx < len(desired_pattern):
+    while idx < desired_pattern_size:
         if idx in stops:
             stops.remove(idx)
             multiplier = multipliers[idx]
 
-            for pattern in patterns:
-                if desired_pattern[idx:].startswith(pattern):
-                    _next = idx + len(pattern)
-                    multipliers[_next] += multiplier
-                    stops.add(_next)
-
-            if idx == 0 and not stops:
-                return 0
+            for size, n in index_starts[idx].items():
+                _next = idx + size
+                multipliers[_next] += (multiplier*n)
+                stops.add(_next)
 
         idx += 1
 
@@ -40,17 +47,17 @@ def match(patterns: Set[str], desired_pattern: str) -> int:
 def main(data: str) -> int:
     raw_patterns, raw_desired = data.split("\n\n")
 
-    patterns = set(raw_patterns.split(", "))
+    patterns = {p: len(p) for p in raw_patterns.split(", ")}
     desired_patterns = raw_desired.splitlines()
 
-    total = 0
-    for d in desired_patterns:
-        total += match(
-            {p for p in patterns if p in d},
-            d
-        )
-
-    return total
+    return reduce(
+        lambda acc, p: acc + match(
+            ((k, v) for k, v in patterns.items() if k in p),
+            p
+        ),
+        desired_patterns,
+        0
+    )
 
 
 if __name__ == "__main__":
