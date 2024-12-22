bc1c44099932bbc1883a8517869346b0002f57d1
taylorperkins <taylorperkins.dev@gmail.com>
2024-12-22 11:27:57 -0600

    Part 1

--- day_22/part_1.py
+++ day_22/part_1.py
@@ -1,9 +1,50 @@
 from utils import time_it, read_input, input_path
 
 
+def step_one(v: int) -> int:
+    return v * 64
+
+def step_two(v: int) -> int:
+    return int(v / 32)
+
+def step_three(v: int) -> int:
+    return v * 2048
+
+def mix(secret_number: int, v: int) -> int:
+    return secret_number ^ v
+
+def prune(secret_number: int):
+    return secret_number % 16777216
+
+
+def get_secret_numbers(secret_number):
+    process = (
+        step_one, step_two, step_three
+    )
+
+    while True:
+        for step in process:
+            result = step(secret_number)
+            secret_number = mix(secret_number, result)
+            secret_number = prune(secret_number)
+
+        yield secret_number
+
+
 @time_it
-def main(data: str) -> str:
-    return data
+def main(data: str) -> int:
+    secret_numbers = [int(line) for line in data.splitlines()]
+
+    total = 0
+    for sn in secret_numbers:
+        gen = get_secret_numbers(sn)
+        new_sn = 0
+        for _ in range(2000):
+            new_sn = next(gen)
+        total += new_sn
+        print(f"{sn}: {new_sn}")
+
+    return total
 
 
 if __name__ == "__main__":
